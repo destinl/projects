4a9f26a44d677618ac47f629910a30bc7266d074
destinl <1374214766@qq.com>
2024-07-21 22:29:59 +0800

    dp

--- new0.cpp
+++ new0.cpp
@@ -1,27 +1,22 @@
+// 1186. 删除一次得到子数组最大和
+// 已解答
+// 中等
+// 相关标签
+// 相关企业
+// 提示
+// 给你一个整数数组，返回它的某个 非空 子数组（连续元素）在执行一次可选的删除操作后，所能得到的最大元素总和。换句话说，你可以从原数组中选出一个子数组，并可以决定要不要从中删除一个元素（只能删一次哦），（删除后）子数组中至少应当有一个元素，然后该子数组（剩下）的元素总和是所有子数组之中最大的。
+
+// 注意，删除一个元素后，子数组 不能为空。
+
 class Solution {
 public:
-    int minimumMoves(vector<vector<int>>& grid) {
-        vector<pair<int, int>> from, to;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j]) {
-                    for (int k = 1; k < grid[i][j]; k++) {
-                        from.emplace_back(i, j);
-                    }
-                } else {
-                    to.emplace_back(i, j);
-                }
-            }
+    int maximumSum(vector<int>& arr) {
+        int dp0 = arr[0], dp1 = 0, res = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            dp1 = max(dp0, dp1 + arr[i]);
+            dp0 = max(dp0, 0) + arr[i];
+            res = max(res, max(dp0, dp1));
         }
-
-        int ans = INT_MAX;
-        do {
-            int total = 0;
-            for (int i = 0; i < from.size(); i++) {
-                total += abs(from[i].first - to[i].first) + abs(from[i].second - to[i].second);
-            }
-            ans = min(ans, total);
-        } while (next_permutation(from.begin(), from.end()));
-        return ans;
+        return res;
     }
 };
